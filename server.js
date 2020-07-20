9cbdafbf22d167f287d7b64f3340dc6e40ed6158
Avi Balsingh <lemonlimewine@gmail.com>
2020-07-20 18:16:12 -0400

    Finalized App

--- server.js
+++ server.js
@@ -1,95 +1,64 @@
-const express = require("express");
-const fs = require( "fs" )
+var express = require("express");
+var fs = require("fs");
+var path = require("path");
 
-const app = express();
-const PORT = 3000;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-// look in 'html' FIRST and serve any static file
-app.use(express.static('public'))
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()); 
 
-function loadNotes(){
-    const notes = JSON.parse( fs.readFileSync( 'notes.json', 'utf8' ) )
-    return notes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+function loadNotes() {
+    var notes = JSON.parse( fs.readFileSync( './db/db.json', 'utf8' ) )
+    return notes;
 }
 
-function saveNotes(){
-    const notesJSON = JSON.stringify( notesList )
-    fs.writeFileSync( 'notes.json', notesJSON )
-}
-
-const notesList = loadNotes()
-console.log( `... starting Node, the notesList = `, notesList )
+var noteList = loadNotes();
 
 
 app.get("/api/notes", function(req, res) {
-    console.log( `[GET /api/notes]` )
-    // send back the list of notes
-    res.send( notesList )
+    console.log(`[GET /api/notes]`)
+    res.send(noteList);
 });
 
-app.get("/api/notes/:id", function(req, res) {
-  // send back the list of notes
-  const noteId = req.params.id
-  console.log( `[GET /api/notes/${noteId}]` )
+function saveNotes() {
+    var notesJSON = JSON.stringify(noteList);
+    fs.writeFileSync ("./db/db.json", notesJSON);
+};
 
-  const singleNote = notesList.filter( note=>note.id == noteId )
-  res.send( singleNote )
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    newNote.id = Date.now()
+    console.log(`[POST/api/notes]`, newNote);
+
+    noteList.push(newNote);
+    saveNotes();
+
+    res.send(newNote);
 });
 
 app.delete("/api/notes/:id", function(req, res) {
-  // send back the list of notes
-  const noteId = req.params.id
-  const updatedNotesList = notesList.filter( note=>note.id !== noteId )
-  notesList = updatedNotesList
-  saveNotes()
-  res.send( { status: true, message: `Deleted note ${noteId}` } )
-});
+    const noteId = req.params.id
 
-app.post( "/api/notes", function( req, res ){
-    // req.body
-    const newNote = req.body
-    console.log( `[POST /api/notes]`, newNote )
-
-    // check this id does not exist yet
-    if( notesList.length &&  notesList.findIndex( note=>note.id == newNote.id )>-1 ){
-        console.log( `x sorry but POST was denied as the note id already exists (id=${newNote.id})` )
-        // could set HTTP response code too [ex. 400 Bad Request]
-        res.status(400).send( { status: false, message: `Sorry that note id already exists. Can't create it.` } )
-    }
-
-    newNote.id = Date.now()
-    notesList.push( newNote )
+    noteList = noteList.filter( note=> note.id != noteId)
+    
     saveNotes()
 
-    console.log( notesList )  
-    res.send( { status: true, message: `Cool beans, we save it for you` } )
+    console.log(`[DELETE/api/notes]`)
+    res.send({id: noteId, message: `Delete successful ${noteId}`, status: true})
 })
 
-app.put( "/api/notes", function( req, res ){
-  // req.body
-  const revisedNote = req.body
-  console.log( `[PUT /api/notes]`, revisedNote )
-
-  if( !revisedNote.id ){
-    console.log( `couldn't update that note, as id was missing!~ quitting` )
-    res.send( { status: false, message: `Missing the id from the update, could not update` } )
-  }
-
-  // find which note to update
-  const noteIdx = notesList.findIndex( note=>note.id == revisedNote.id )
-  notesList[noteIdx] = revisedNote
-  saveNotes()
-  
-  console.log( notesList )  
-  res.send( { status: true, message: `Cool beans, we updated note #${revisedNote.id}` } )
-})
-
-
-// Listener
-// ===========================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});0
+app.listen (PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
